a66a9e476e677ccdfe0032c26f0f97d4cabf26f4
ajipon-44 <63995620+ajipon-44@users.noreply.github.com>
2024-01-08 17:09:12 +0900

    MemberNFT:NFT作成のテスト実装

--- test/MemberNFT.js
+++ test/MemberNFT.js
@@ -6,10 +6,13 @@ describe("MemberNFTコントラクト", function () {
   let memberNFT;
   const name = "MemberNFT";
   const symbol = "MEM";
+  const tokenURI1 = "hoge1";
+  const tokenURI2 = "hoge2";
   let owner;
+  let addr1;
 
   beforeEach(async function () {
-    [owner] = await ethers.getSigners(); // ethersのアカウント群から最初のアカウントを取得
+    [owner, addr1] = await ethers.getSigners(); // ethersのアカウント群から最初のアカウントを取得
     MemberNFT = await ethers.getContractFactory("MemberNFT"); // MemberNFTコントラクトの抽象化
     memberNFT = await MemberNFT.deploy(); // 抽象化されたMemberNFTをデプロイしてオブジェクト化
     await memberNFT.deployed(); // デプロイ完了を待つ
@@ -21,5 +24,20 @@ describe("MemberNFTコントラクト", function () {
   });
   it("デプロイアドレスがownerに設定されるべき", async function () {
     expect(await memberNFT.owner()).to.equal(owner.address); // owner.addressでSignerWithAddressオブジェクトからアドレスを取得
+  });
+  it("ownerはNFT作成できるべき", async function () {
+    await memberNFT.nftMint(addr1.address, tokenURI1);
+    expect(await memberNFT.ownerOf(1)).to.equal(addr1.address);
+  });
+  it("NFT作成のたびにtokenIdがインクリメントされるべき", async function () {
+    await memberNFT.nftMint(addr1.address, tokenURI1);
+    await memberNFT.nftMint(addr1.address, tokenURI2);
+    expect(await memberNFT.tokenURI(1)).to.equal(tokenURI1);
+    expect(await memberNFT.tokenURI(2)).to.equal(tokenURI2);
+  });
+  it("owner以外はNFT作成に失敗すべき", async function () {
+    await expect(
+      memberNFT.connect(addr1).nftMint(addr1.address, tokenURI1)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
   });
 });
