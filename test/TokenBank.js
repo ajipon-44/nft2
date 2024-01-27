96a6abc58bc1a1906831002c3561ddc3cb547466
ajipon-44 <63995620+ajipon-44@users.noreply.github.com>
2024-01-27 10:50:28 +0900

    TokenBank:トークン預入のテスト

--- test/TokenBank.js
+++ test/TokenBank.js
@@ -9,10 +9,11 @@ describe("TokenBankコントラクト", function () {
   let owner;
   let addr1;
   let addr2;
+  let addr3;
   const zeroAddress = "0x0000000000000000000000000000000000000000";
 
   beforeEach(async function () {
-    [owner, addr1, addr2] = await ethers.getSigners(); // ethersのアカウント群から最初のアカウントを取得
+    [owner, addr1, addr2, addr3] = await ethers.getSigners(); // ethersのアカウント群から最初のアカウントを取得
     TokenBank = await ethers.getContractFactory("TokenBank"); // MemberNFTコントラクトの抽象化
     tokenBank = await TokenBank.deploy(name, symbol); // 抽象化されたMemberNFTをデプロイしてオブジェクト化
     await tokenBank.deployed(); // デプロイ完了を待つ
@@ -29,6 +30,9 @@ describe("TokenBankコントラクト", function () {
     it("ownerに総額が割り当てられるべき", async function () {
       const ownerBalance = await tokenBank.balanceOf(owner.address);
       expect(await tokenBank.totalSupply()).to.equal(ownerBalance);
+    });
+    it("預かっているTokenの総額が0であるべき", async function () {
+      expect(await tokenBank.bankTotalDeposit()).to.equal(0);
     });
   });
 
@@ -67,6 +71,42 @@ describe("TokenBankコントラクト", function () {
       await expect(tokenBank.connect(addr1).transfer(addr2.address, 100))
         .to.emit(tokenBank, "TokenTransfer")
         .withArgs(addr1.address, addr2.address, 100);
+    });
+  });
+
+  describe("Bankトランザクション", function () {
+    beforeEach(async function () {
+      await tokenBank.transfer(addr1.address, 500);
+      await tokenBank.transfer(addr2.address, 200);
+      await tokenBank.transfer(addr3.address, 100);
+      await tokenBank.connect(addr1).deposit(100);
+      await tokenBank.connect(addr2).deposit(200);
+    });
+
+    it("token預入が実行できるべき", async function () {
+      const addr1Balance = await tokenBank.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(400);
+      const addr1BankBalance = await tokenBank.bankBalanceOf(addr1.address);
+      expect(addr1BankBalance).to.equal(100);
+    });
+    it("預入後にもTokenを移転できるべき", async function () {
+      const startAddr1Balance = await tokenBank.balanceOf(addr1.address);
+      const startAddr2Balance = await tokenBank.balanceOf(addr2.address);
+
+      // 任意のアドレスを指定してコントラクトを実行したい場合はconnectして，アカウント情報を引数に渡す
+      await tokenBank.connect(addr1).transfer(addr2.address, 100);
+
+      const endAddr1Balance = await tokenBank.balanceOf(addr1.address);
+      const endAddr2Balance = await tokenBank.balanceOf(addr2.address);
+
+      // .subで引き算する, addで足し算する
+      expect(endAddr1Balance).to.equal(startAddr1Balance.sub(100));
+      expect(endAddr2Balance).to.equal(startAddr2Balance.add(100));
+    });
+    it("預入後には'TokenDeposit'イベントが発行されるべき", async function () {
+      await expect(tokenBank.connect(addr1).deposit(100))
+        .to.emit(tokenBank, "TokenDeposit")
+        .withArgs(addr1.address, 100);
     });
   });
 });
